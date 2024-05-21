5c067a8ca69bf851857921af8f46dd8d975f1ba1
Niraj Paudel <itsmenirajpaudel@gmail.com>
2024-05-21 11:07:12 +0545

    Add enhanced example in main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,19 @@
 use term_color_support::ColorSupport;
 
 fn main() {
-    println!("Output {:?}", ColorSupport::stdout());
-    println!("Output {:?}", ColorSupport::stderr());
+    // Get color support information for stdout
+    let stdout_info = ColorSupport::stdout();
+    println!("Output for stdout:");
+    println!("  Level: {:?}", stdout_info.level);
+    println!("  Basic Color Support: {:?}", stdout_info.has_basic);
+    println!("  256-color Support: {:?}", stdout_info.has_256);
+    println!("  True Color (16 million colors) Support: {:?}", stdout_info.has_16m);
+
+    // Get color support information for stderr
+    let stderr_info = ColorSupport::stderr();
+    println!("Output for stderr:");
+    println!("  Level: {:?}", stderr_info.level);
+    println!("  Basic Color Support: {:?}", stderr_info.has_basic);
+    println!("  256-color Support: {:?}", stderr_info.has_256);
+    println!("  True Color (16 million colors) Support: {:?}", stderr_info.has_16m);
 }
